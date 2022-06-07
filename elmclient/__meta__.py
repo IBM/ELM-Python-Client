7940ca25e45b3782f606b5a94d44dec7380a111e
Ian Barnard <ian.barnard@uk.ibm.com>
2022-06-07 08:38:09 +0100

    Bump version: 0.5.0 → 0.6.0

--- elmclient/__meta__.py
+++ elmclient/__meta__.py
@@ -9,7 +9,7 @@
 
 app = 'elmoslcquery'
 description = 'Commandline OSLC query for ELM'
-version = '0.5.0'
+version = '0.6.0'
 license = 'MIT'
 author_name = 'Ian Barnard'
 author_mail = 'ian.barnard@uk.ibm.com'

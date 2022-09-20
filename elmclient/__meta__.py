f959af04112c198b4939287ba2793c5df59c8a17
Ian Barnard <ian.barnard@uk.ibm.com>
2022-09-20 12:56:51 +0100

    removed Python 3.9 requirement

--- elmclient/__meta__.py
+++ elmclient/__meta__.py
@@ -9,7 +9,7 @@
 
 app = 'elmoslcquery'
 description = 'Commandline OSLC query for ELM'
-version = '0.9.3'
+version = '0.9.4'
 license = 'MIT'
 author_name = 'Ian Barnard'
 author_mail = 'ian.barnard@uk.ibm.com'
